682fe9761dd82d5717eaa7b5c71ba63fed543b97
Feng <feng.guo2001@gmail.com>
2022-10-20 13:22:05 -0400

    Time: 33 ms (7.51%), Space: 48.2 MB (13.00%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.java
+++ 12-integer-to-roman/12-integer-to-roman.java
@@ -1,46 +1,32 @@
 class Solution {
     public String intToRoman(int num) {
         String roman = "";
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1000, "M");
+        map.put(500, "D");
+        map.put(100, "C");
+        map.put(50, "L");
+        map.put(10, "X");
+        map.put(5, "V");
+        map.put(1, "I");
+        int divisor = 1000;
+        
         while (num != 0) {
-            if (num >= 1000) {
-                roman += "M";
-                num -= 1000;
-            } else if (num >= 900) {
-                roman += "CM";
-                num -= 900;
-            } else if (num >= 500) {
-                roman += "D";
-                num -= 500;
-            } else if (num >= 400) {
-                roman += "CD";
-                num -= 400; 
-            } else if (num >= 100) {
-                roman += "C";
-                num -= 100;
-            } else if (num >= 90) {
-                roman += "XC";
-                num -= 90;
-            } else if (num >= 50) {
-                roman += "L";
-                num -= 50;
-            } else if (num >= 40) {
-                roman += "XL";
-                num -= 40;
-            } else if (num >= 10) {
-                roman += "X";
-                num -= 10;
-            } else if (num >= 9) {
-                roman += "IX";
-                num -= 9;
-            } else if (num >= 5) {
-                roman += "V";
-                num -= 5;
-            } else if (num >= 4) {
-                roman += "IV";
-                num -= 4;
-            } else if (num >= 1) {
-                roman += "I";
-                num -= 1;
+            int smaller = divisor / 10;
+            if (num >= divisor) {
+                roman += map.get(divisor);
+                num -= divisor;
+            } else if (num >= (divisor - smaller)) {
+                roman += map.get(smaller) + map.get(divisor);
+                num -= (divisor - smaller);
+            } else if (num >= divisor/2) {
+                roman += map.get(divisor/2);
+                num  -= (divisor/2);
+            } else if (num >= (divisor/2) - smaller) {
+                roman += map.get(smaller) + map.get(divisor/2);
+                num  -= ((divisor/2) - smaller);
+            } else {
+                divisor = smaller;
             }
         }
         
