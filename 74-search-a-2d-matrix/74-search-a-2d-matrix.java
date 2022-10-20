01456eb9792b5f7867e2d363cb6f4987c4586822
Feng <feng.guo2001@gmail.com>
2022-10-20 12:46:11 -0400

    Time: 1 ms (44.10%), Space: 43.1 MB (18.49%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,14 +1,9 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int rows = matrix.length;
-        // if (rows == 0) {
-        //     return false;
-        // }
         int columns = matrix[0].length;
-        
         int left = 0;
         int right = rows*columns-1;
-        
         
         while (left < right && matrix[left/columns][left%columns] != target) {
             int mid = (left+right)/2;
@@ -20,15 +15,6 @@ class Solution {
                 right = mid;
             }
         }
-        // System.out.println(left);
-        // System.out.println(left/columns);
-        // System.out.println(left%columns);
         return matrix[left/columns][left%columns] == target;
-        
-        // if (nums[left] == target) {
-        //     return left;
-        // } else {
-        //     return -1;
-        // }
     }
 }
