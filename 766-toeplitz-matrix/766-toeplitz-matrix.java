353d1aa28cb2442338ead52a0768cc77bc6a6e3a
Feng <feng.guo2001@gmail.com>
2022-10-31 11:20:32 -0400

    Time: 1 ms (93.83%), Space: 42.6 MB (86.02%) - LeetHub

--- 766-toeplitz-matrix/766-toeplitz-matrix.java
+++ 766-toeplitz-matrix/766-toeplitz-matrix.java
@@ -1,18 +1,16 @@
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
         for(int i=0; i<matrix.length; i++) {
-            int start = matrix[i][0];
             for (int j=1; j<matrix[0].length; j++) {
-                if (i+j < matrix.length && matrix[i+j][j] != start) {
+                if (i+j < matrix.length && matrix[i+j][j] != matrix[i][0]) {
                     return false;
                 }
             }
         }
         
         for(int i=1; i<matrix[0].length; i++) {
-            int start = matrix[0][i];
             for (int j=1; j<matrix.length; j++) {
-                if (i+j < matrix[0].length && matrix[j][i+j] != start) {
+                if (i+j < matrix[0].length && matrix[j][i+j] != matrix[0][i]) {
                     return false;
                 }
             }
