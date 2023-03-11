e173e1013281e1219656d81b9e69bb813f072249
Feng <feng.guo2001@gmail.com>
2023-03-11 00:01:16 -0500

    Time: 0 ms (100.00%), Space: 44.2 MB (15.03%) - LeetHub

---
+++ 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode next; ListNode(int
+ * x) { val = x; } }
+ */
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode left; TreeNode
+ * right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+
+  private ListNode head;
+
+  private int findSize(ListNode head) {
+    ListNode ptr = head;
+    int c = 0;
+    while (ptr != null) {
+      ptr = ptr.next;  
+      c += 1;
+    }
+    return c;
+  }
+
+  private TreeNode convertListToBST(int l, int r) {
+    // Invalid case
+    if (l > r) {
+      return null;
+    }
+
+    int mid = (l + r) / 2;
+
+    // First step of simulated inorder traversal. Recursively form
+    // the left half
+    TreeNode left = this.convertListToBST(l, mid - 1);
+
+    // Once left half is traversed, process the current node
+    TreeNode node = new TreeNode(this.head.val);
+    node.left = left;
+
+    // Maintain the invariance mentioned in the algorithm
+    this.head = this.head.next;
+
+    // Recurse on the right hand side and form BST out of them
+    node.right = this.convertListToBST(mid + 1, r);
+    return node;
+  }
+
+  public TreeNode sortedListToBST(ListNode head) {
+    // Get the size of the linked list first
+    int size = this.findSize(head);
+
+    this.head = head;
+
+    // Form the BST now that we know the size
+    return convertListToBST(0, size - 1);
+  }
+}
