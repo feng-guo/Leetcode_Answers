916ca0072c0ec4ac100823b1b4f65474d57b47ad
Feng <feng.guo2001@gmail.com>
2023-03-22 17:40:55 -0400

    Time: 1 ms (98.75%), Space: 44.6 MB (69.52%) - LeetHub

--- 57-insert-interval/57-insert-interval.java
+++ 57-insert-interval/57-insert-interval.java
@@ -1,71 +1,32 @@
 class Solution {
-    // Returns true if the intervals a and b have a common element.
-    boolean doesIntervalsOverlap(int[] a, int[] b) {
-        return Math.min(a[1], b[1]) - Math.max(a[0], b[0]) >= 0;
-    }
-
-    // Return the interval having all the elements of intervals a and b.
-    int[] mergeIntervals(int[] a, int[] b) {
-        int[] newInterval = {Math.min(a[0], b[0]), Math.max(a[1], b[1])};
-        return newInterval;
-    }
-
-    int UpperBound(int[][] intervals, int[] newInterval) {
-        if (intervals.length == 0) {
-            // If the list is empty, just insert the newInterval at the 0th position.
-            return 0;
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> list = new ArrayList<>();
+        boolean placed = false;
+        for (int[] interval: intervals) {
+                if (interval[0] > newInterval[1]) {
+                    if (!placed) {
+                        list.add(newInterval);
+                        placed = true;
+                    }
+                    list.add(interval);
+                } else if (interval[1] >= newInterval[0] && interval[1] <= newInterval[1]) {
+                    newInterval[0] = Math.min(interval[0], newInterval[0]);
+                } else if (interval[0] >= newInterval[0] && interval[0] <= newInterval[1]) {
+                    newInterval[1] = Math.max(interval[1], newInterval[1]);
+                } else if (newInterval[0] >= interval[0] && newInterval[1] <= interval[1]) {
+                    list.add(interval);
+                    placed = true;
+                } else {
+                    list.add(interval);
+                }
         }
-
-        int start = 0, end = intervals.length - 1;
-        int ans = intervals.length;
-        while (start <= end) {
-            int mid = (start + end) / 2;
-
-            // If the start value is greater than the newInterval
-            // This could be the position, so store it but keep looking on the left side.
-            if (intervals[mid][0] > newInterval[0]) {
-                ans = mid;
-                end = mid - 1;
-            } else {
-                // Search on the right side of mid.
-                start = mid + 1;
-            }
-        }
-
-        return ans;
-    }
-
-    // Insert the interval newInterval, into the list interval keeping the sorting order intact.
-    int[][] insertInterval(int[][] intervals, int[] newInterval) {
-        List<int[]> list = new ArrayList<>(Arrays.asList(intervals));
-        int index = UpperBound(intervals, newInterval);
-
-        if (index != intervals.length) {
-            list.add(index, newInterval);
-        } else {
+        if (!placed) {
             list.add(newInterval);
         }
-
-        return list.toArray(new int[list.size()][2]);
-    }
-
-    public int[][] insert(int[][] intervals, int[] newInterval) {
-        // Insert the interval first before merge processing.
-        intervals = insertInterval(intervals, newInterval);
-
-        List<int[]> answer = new ArrayList<>();
-        for (int i = 0; i < intervals.length; i++) {
-            int[] currInterval = {intervals[i][0], intervals[i][1]};
-            // Merge until the list gets exhausted or no overlap is found.
-            while (i < intervals.length && doesIntervalsOverlap(currInterval, intervals[i])) {
-                currInterval = mergeIntervals(currInterval, intervals[i]);
-                i++;
-            }
-            // Decrement to ensure we don't skip the interval due to outer for-loop incrementing.
-            i--;
-            answer.add(currInterval);
+        int[][] res = new int[list.size()][2];
+        for (int i=0; i<list.size(); i++) {
+            res[i] = list.get(i);
         }
-
-        return answer.toArray(new int[answer.size()][2]);
+        return res;
     }
 }
