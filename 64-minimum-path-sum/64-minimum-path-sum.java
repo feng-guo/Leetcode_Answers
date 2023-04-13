4a34c216366018bcc17c9a28b6fc24f8afe5e710
Feng <feng.guo2001@gmail.com>
2023-04-13 11:52:12 -0400

    Time: 2 ms (81.53%), Space: 45.1 MB (73.59%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -1,15 +1,19 @@
-public class Solution {
+class Solution {
     public int minPathSum(int[][] grid) {
-        for (int i = grid.length - 1; i >= 0; i--) {
-            for (int j = grid[0].length - 1; j >= 0; j--) {
-                if(i == grid.length - 1 && j != grid[0].length - 1)
-                    grid[i][j] = grid[i][j] +  grid[i][j + 1];
-                else if(j == grid[0].length - 1 && i != grid.length - 1)
-                    grid[i][j] = grid[i][j] + grid[i + 1][j];
-                else if(j != grid[0].length - 1 && i != grid.length - 1)
-                    grid[i][j] = grid[i][j] + Math.min(grid[i + 1][j],grid[i][j + 1]);
+        
+        for (int i=0; i<grid.length; i++) {
+            for (int j=0; j<grid[0].length; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                } else if (i == 0) {
+                    grid[i][j] += grid[i][j-1];
+                } else if (j == 0) {
+                    grid[i][j] += grid[i-1][j];
+                } else {
+                    grid[i][j] += Math.min(grid[i][j-1], grid[i-1][j]);
+                }
             }
         }
-        return grid[0][0];
+        return grid[grid.length-1][grid[0].length-1];
     }
 }
