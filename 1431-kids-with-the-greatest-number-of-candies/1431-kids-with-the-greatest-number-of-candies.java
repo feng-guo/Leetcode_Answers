29ad8f020a52c2acb0a2d70bcf563a909041bae8
Feng <feng.guo2001@gmail.com>
2023-04-17 21:35:58 -0400

    Time: 1 ms (81.10%), Space: 42.8 MB (23.93%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -1,17 +1,13 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        // Find out the greatest number of candies among all the kids.
-        int maxCandies = 0;
-        for (int candy : candies) {
-            maxCandies = Math.max(candy, maxCandies);
+        int max = 0;
+        for (int candy: candies) {
+            max = Math.max(max, candy);
         }
-        // For each kid, check if they will have greatest number of candies
-        // among all the kids.
-        List<Boolean> result = new ArrayList<>();
-        for (int candy : candies) {
-            result.add(candy + extraCandies >= maxCandies);
+        List<Boolean> list = new ArrayList<>();
+        for (int i=0; i<candies.length; i++) {
+            list.add(candies[i] + extraCandies >= max);
         }
-
-        return result;
+        return list;
     }
 }
