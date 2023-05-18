7a3d37ff6f873b58c47b6915adf6b43e6e7e6fa9
Feng <feng.guo2001@gmail.com>
2023-05-17 22:07:42 -0400

    Time: 30 ms (16.63%), Space: 88.6 MB (34.44%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
@@ -1,34 +1,15 @@
 class Solution {
-    Set<Integer> starts = new HashSet<>();
-    Set<Integer> visited = new HashSet<>();
-    Map<Integer, Set<Integer>> map = new HashMap<>();
-    
     public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
-        for (int i=0; i<n; i++) {
-            map.put(i, new HashSet<>());
-        }
+        Set<Integer> visitable = new HashSet<>();
         for (List<Integer> edge: edges) {
-            map.get(edge.get(0)).add(edge.get(1));
+            visitable.add(edge.get(1));
         }
+        List<Integer> res = new ArrayList<>();
         for (int i=0; i<n; i++) {
-            if (visited.add(i)) {
-                starts.add(i);
-                dfs(i);
+            if (visitable.add(i)) {
+                res.add(i);
             }
         }
-        List<Integer> list = new ArrayList<>();
-        for (int i: starts) {
-            list.add(i);
-        }
-        return list;
-    }
-    
-    public void dfs(int current) {
-        for (int i: map.get(current)) {
-            starts.remove(i);
-            if (visited.add(i)) {
-                dfs(i);
-            }
-        }
+        return res;
     }
 }
