1669b31721b571f23bb6d0bb88923767bde6f288
Feng <feng.guo2001@gmail.com>
2024-03-15 20:40:45 -0400

    Time: 614 ms (71.19%), Space: 21.9 MB (97.79%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -12,16 +12,8 @@ class Solution:
                 ones += 1
             if zeros == ones:
                 res = i + 1
-            else:
+            elif zeros-ones not in diff_map:
                 diff_map[zeros-ones] = i
-        
-        zeros = 0
-        ones = 0
-        for i, num in enumerate(nums):
-            if num == 0:
-                zeros += 1
             else:
-                ones += 1
-            if zeros != ones:
-                res = max(res, diff_map[zeros-ones] - i)
+                res = max(res, i - diff_map[zeros-ones])
         return res
