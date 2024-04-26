65acaaf6dc06362092d2766fb94e318a4f2ff609
Feng <feng.guo2001@gmail.com>
2024-04-25 22:28:51 -0400

    Time: 232 ms (65.49%), Space: 20 MB (80.97%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
@@ -1,8 +1,22 @@
 class Solution:
     def minFallingPathSum(self, grid: List[List[int]]) -> int:
-        for i in range(1, len(grid)):
+        prev1 = (0, -1)
+        prev2 = (0, -1)
+        for i in range(0, len(grid)):
+            min1 = None
+            min2 = None
             for j in range(len(grid)):
-                grid[i][j] += min(grid[i-1][:j] + grid[i-1][j+1:])
+                if j != prev1[1]:
+                    grid[i][j] += prev1[0]
+                else:
+                    grid[i][j] += prev2[0]
+                if not min1 or grid[i][j] <= min1[0]:
+                    min2 = min1
+                    min1 = (grid[i][j], j)
+                elif not min2 or min1[0] < grid[i][j] < min2[0]:
+                    min2 = (grid[i][j], j)
+            prev1 = min1
+            prev2 = min2
         
-        return min(grid[-1])
+        return prev1[0]
         
