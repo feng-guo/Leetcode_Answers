690b846d80260c6ec8dd0ee84c72d64782813cfa
Feng <feng.guo2001@gmail.com>
2024-08-11 20:06:46 -0400

    Time: 96 ms (71.34%), Space: 17.9 MB (16.11%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,13 +1,11 @@
-from collections import defaultdict
+from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        freq_map = defaultdict(int)
-        for num in nums:
-            freq_map[num] += 1
+        count = Counter(nums)
         
         res = []
-        for key in freq_map:
-            if freq_map[key] > len(nums)//3:
+        for key in count:
+            if count[key] > len(nums)//3:
                 res.append(key)
         
         return res
